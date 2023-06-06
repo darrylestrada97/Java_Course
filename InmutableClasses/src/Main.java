06ed78d1546e0a669deff9f9deecf4194a8dc788
Darryl Estrada <darrylestrada97@gmail.com>
2023-06-06 12:40:35 +0200

    Person with constructor.

--- InmutableClasses/src/Main.java
+++ InmutableClasses/src/Main.java
@@ -1,5 +1,26 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+
+//        Person jane  = new Person();
+//        jane.setName("Jane");
+//        Person jin = new Person();
+//        jin.setName("Jin");
+//        Person joe = new Person();
+//        joe.setName("Joe");
+//        Person John  = new Person();
+//        John.setName("John");
+//        John.setKids( new Person[]{jane,jin,joe});
+//        John.setDob("19/02/1990");
+//        System.out.println(John);
+
+        Person jane = new Person("Jane","01/01/1930");
+        Person jim = new Person("Jim","02/02/1932");
+        Person joe = new Person("Joe", "04/04/1930");
+        Person[] JohnKinds = {jane,jim,joe};
+        Person john = new Person("John", "05/05/1900", JohnKinds);
+        System.out.println(john);
+
+
     }
 }
